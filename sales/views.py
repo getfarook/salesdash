6296d2fe5e748e449c9b0f6b81265719eaee5528
Farook Kuruvanna <getfarook@gmail.com>
2018-04-20 20:44:15 +0530

    test commit
    test commit

--- sales/views.py
+++ sales/views.py
@@ -17,6 +17,7 @@ from django.http import JsonResponse
 from django.core import serializers
 
 # Create your views here.
+# test comment
 
 class JsTestView(generic.TemplateView):
     template_name = 'sales/jstest.html'
